22313418371e484ce791bfd2a96e56758ce0b9a7
oneofthezombies <hunhoekim@gmail.com>
2024-09-27 16:04:01 +0900

    build: refactor proto build

--- packages/proto/scripts/build.mjs
+++ packages/proto/scripts/build.mjs
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
 import { shell } from './share/utils.mjs'
+import fs from 'node:fs'
+import path from 'node:path'
 
 if (process.argv[1] === import.meta.filename) {
   shell('mkdir -p dist/src')
+  const jsPath = 'dist/src/generated.js'
   shell(
-    'npx pbjs --target static-module --wrap commonjs --es6 --out dist/src/generated.cjs res/goat_rpc.proto'
+    `npx pbjs --target static-module --wrap commonjs --es6 --out ${jsPath} res/goat_rpc.proto`
   )
-  shell(
-    'npx pbjs --target static-module --wrap commonjs --es6 res/goat_rpc.proto | npx pbts --out dist/src/generated.d.ts -'
-  )
+  shell(`npx npx pbts --out dist/src/generated.d.ts ${jsPath}`)
+  const cjsPath = path.format({
+    ...path.parse(jsPath),
+    base: undefined,
+    ext: '.cjs',
+  })
+  fs.renameSync(jsPath, cjsPath)
 }
