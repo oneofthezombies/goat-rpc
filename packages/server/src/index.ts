58c833cb1db88453db98cab3e18b4ade2750c969
oneofthezombies <hunhoekim@gmail.com>
2024-09-29 23:26:47 +0900

    wip: server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,34 @@
 import proto from '@goat-rpc/proto'
-console.log(proto)
-console.log(proto.goat_rpc.UnaryCall)
+import { WebSocketServer } from 'ws'
+
+const server = new WebSocketServer({ port: 8080 })
+server.on('connection', (socket, request) => {
+  console.log(socket, request)
+  socket.on('close', (code, reason) => {
+    console.log(code, reason)
+  })
+  socket.on('error', (error) => {
+    console.log(error)
+  })
+  socket.on('message', (data, isBinary) => {
+    console.log(data.toString(), isBinary)
+  })
+  socket.on('open', () => {
+    console.log('open')
+  })
+  socket.on('ping', (data) => {
+    console.log(data)
+  })
+})
+server.on('close', () => {
+  console.log('close')
+})
+server.on('error', (error) => {
+  console.log(error)
+})
+server.on('headers', (headers, request) => {
+  console.log(headers, request)
+})
+server.on('listening', () => {
+  console.log('listening')
+})
